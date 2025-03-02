58b68294cd9bb6f9b77b360d004f1b8d22116e14
John Gomez <johngomez45@Johns-MacBook-Pro.local>
2025-03-01 19:24:16 -0500

    created task model

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,11 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(TaskApp());
 }
+
+class Task {
+  String name;
+  bool isCompleted;
+
+  Task({required this.name, this.isCompleted = false});
+}
+
