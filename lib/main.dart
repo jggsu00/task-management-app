7834b77ef3bd11f2139b5d4429ccb68447475a11
John Gomez <johngomez45@Johns-MacBook-Pro.local>
2025-03-01 19:35:23 -0500

    main app stucture

--- lib/main.dart
+++ lib/main.dart
@@ -11,3 +11,12 @@ class Task {
   Task({required this.name, this.isCompleted = false});
 }
 
+class TaskApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TaskListScreen(),
+    );
+  }
+}
